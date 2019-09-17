e927b6f78dd09c70db95fe5431467fb047913296
Nikolaj Majorov <nikolaj@majorov.biz>
2019-09-17 18:41:50 +0200

    add code-fist approach

---
+++ code-first-approach/src/main/kotlin/io/fabric8/quickstarts/expences/Entities.kt
@@ -0,0 +1,25 @@
+package io.fabric8.quickstarts.expences
+
+import java.math.BigDecimal
+import java.time.LocalDateTime
+
+
+//https://spring.io/guides/tutorials/spring-boot-kotlin/
+
+class User(
+        var login: String,
+        var firstname: String,
+        var lastname: String,
+        var id: Long? = null
+)
+
+
+class Expence (
+
+        var description: String,
+        var date: LocalDateTime,
+        var sum: BigDecimal,
+       // @ManyToOne var author: User,
+        var addedAt: LocalDateTime = LocalDateTime.now()
+)
+        //@Id @GeneratedValue var id: Long? = null)
