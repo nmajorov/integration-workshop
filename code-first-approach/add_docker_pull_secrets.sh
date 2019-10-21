6c03238edbefe402266e650e39eaeff9a325bd80
Nikolaj Majorov <nikolaj@majorov.biz>
2019-10-21 22:25:45 +0200

    move code directory to the separate repo

--- code-first-approach/add_docker_pull_secrets.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-#add secret for redhat.io container registry
-
-DOCKER_CONF=$HOME/.docker/config.json
-
- oc create secret generic rhpullsecret \
-                --from-file=.dockerconfigjson=$DOCKER_CONF \
-                --type=kubernetes.io/dockerconfigjson
